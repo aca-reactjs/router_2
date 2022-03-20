884a982582d755489a0c986a7ae5bb0e6fb8f73d
sargis <abovyan.sargis90@gmail.com>
2022-03-20 13:33:27 +0400

    add use routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Link, Outlet, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useNavigate,
+  useRoutes,
+} from "react-router-dom";
 import { MainLayout } from "./components";
 import Countries from "./pages/Countries";
 import Country from "./pages/Country";
@@ -7,19 +14,34 @@ import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="countries" element={<Countries />} />
-          <Route path="countries/:code" element={<Country />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="*" element={<h1>not found</h1>} />
-        </Route>
-      </Routes>
-    </div>
-  );
+  return useRoutes([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/countries",
+          element: <Countries />,
+        },
+        {
+          path: "countries/:code",
+          element: <Country />,
+        },
+        {
+          path: "favorites",
+          element: <Favorites />,
+        },
+        {
+          path: "*",
+          element: <h1>not found</h1>,
+        },
+      ],
+    },
+  ]);
 }
 
 export default App;
