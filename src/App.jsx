55f8ef05e59bd2dbe26af031db1b53b630862d3d
sargis <abovyan.sargis90@gmail.com>
2022-03-20 11:17:12 +0400

    ci router add countries

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, Outlet, Route, Routes, useNavigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Countries from "./pages/Countries";
+import Country from "./pages/Country";
 import Home from "./pages/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="countries" element={<Countries />} />
+          <Route path="countries/:code" element={<Country />} />
         </Route>
       </Routes>
     </div>
--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Countries() {
+  const [countries, setCountries] = useState([]);
+  const navigate = useNavigate();
+
+  const handleListItemClick = (code) => () => {
+    navigate(`${code}`);
+  };
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then(setCountries);
+  }, []);
   return (
     <div>
-      <h1>hello</h1>
+      {countries.map((el) => (
+        <li onClick={handleListItemClick(el.cca2)} key={el.cca2}>
+          {el.name.common}
+        </li>
+      ))}
     </div>
   );
 }
