e58650b12fd3b7d9772c0ec26d76358f4001e6b5
sargis <abovyan.sargis90@gmail.com>
2022-03-20 10:49:18 +0400

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/second");
+  };
+
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={handleButtonClick}>second</button>
     </div>
   );
 }
@@ -16,15 +24,24 @@ function Second() {
   );
 }
 
-function Router({ path, element }) {
-  return null;
+function Layout() {
+  return (
+    <div>
+      <nav></nav>
+      <Outlet />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Router path="/" element={<Home />} />
-      <Router path="/second" element={<Second />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="second" element={<Second />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
