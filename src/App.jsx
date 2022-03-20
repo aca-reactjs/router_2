425d394b5b5c3d182bce17f67cdff822c827b8d3
sargis <abovyan.sargis90@gmail.com>
2022-03-20 11:00:01 +0400

    add countries route

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
 import { useState } from "react";
-import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
-
-function Home() {
-  const navigate = useNavigate();
-
-  const handleButtonClick = () => {
-    navigate("/second");
-  };
-
-  return (
-    <div>
-      <h1>Home</h1>
-      <button onClick={handleButtonClick}>second</button>
-    </div>
-  );
-}
-
-function Second() {
-  return (
-    <div>
-      <h1>Second</h1>
-    </div>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      <nav></nav>
-      <Outlet />
-    </div>
-  );
-}
+import { Link, Outlet, Route, Routes, useNavigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Countries from "./pages/Countries";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -39,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="second" element={<Second />} />
+          <Route path="countries" element={<Countries />} />
         </Route>
       </Routes>
     </div>
